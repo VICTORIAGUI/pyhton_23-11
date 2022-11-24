ad40a46643a216fa444bdbb407ac2b09f8d0f81d
VICTORIAGUI <victoriagui.santos@gmail.com>
2022-11-23 23:39:57 -0300

    corringindo sintaxe

--- desafio03.py
+++ desafio03.py
@@ -8,9 +8,9 @@
 # nas últimas 24 hrs.
 
 
-idade = int(input('Qual a sua idade?'))
-peso = int(input('Quanto você pesa?'))
-sono = int(input('Quantas horas você dormiu?'))
+idade = int(input('Qual a sua idade? '))
+peso = int(input('Quanto você pesa? '))
+sono = int(input('Quantas horas você dormiu? '))
 
 if idade >= 16 and idade < 69 and peso >= 50 and sono >= 6:
     print('Parabéns! Você está apto para doar sangue, ajude uma vida!')
